5590568913f850b79d73103cd25697ad22a5d17e
sridhar <sridharmay1@gmail.com>
2020-06-18 15:14:39 +0530

    CodeCoverage img added

--- Opportunity-Mgmt/src/main/java/com/accolite/opportunity/OpportunityMgmtApplication.java
+++ Opportunity-Mgmt/src/main/java/com/accolite/opportunity/OpportunityMgmtApplication.java
@@ -1,11 +1,7 @@
 package com.accolite.opportunity;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OpportunityMgmtApplication {
@@ -14,16 +10,16 @@ public class OpportunityMgmtApplication {
 		SpringApplication.run(OpportunityMgmtApplication.class, args);
 	}
 
-	@Value("app.allowed-origins")
-	private String[] allowedOrigins;
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(allowedOrigins);
-			}
-		};
-	}
+//	@Value("app.allowed-origins")
+//	private String[] allowedOrigins;
+//
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins(allowedOrigins);
+//			}
+//		};
+//	}
 
 }
